30bbbaa11f80daf2cf8fc7b9a734d3b12b09f3a7
Stefan Kessel <kessel.stefan1@gmail.com>
2020-06-21 17:59:49 +0200

    init

---
+++ src/uni/Maze.java
@@ -0,0 +1,13 @@
+package uni;
+
+public class Maze {
+	
+	public static void main(String [] args) {
+		
+		int i = 0;
+		do {
+			System.out.println("i ist: " + i);
+			i++;
+		}while(i < 10);
+	}
+}
